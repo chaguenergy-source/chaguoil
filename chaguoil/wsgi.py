7ef3cbefe39c28b918330afacdcc444fb0d7e620
MusaJaphet <musajaphet790@gmail.com>
2025-10-09 09:33:29 +0300

    changes

--- chaguoil/wsgi.py
+++ chaguoil/wsgi.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+# Import hi imelazimishwa ili kupakia storages mapema, kabla ya Django kutafuta DEFAULT_FILE_STORAGE.
+import storages 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chaguoil.settings')
 
